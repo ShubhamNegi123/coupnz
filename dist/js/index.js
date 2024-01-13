8a7646ca949d3c295ec851ee7005ea65ec8c0f2d
Shubham Negi <95840309+ShubhamNegi123@users.noreply.github.com>
2024-01-13 17:01:02 +0530

    Add files via upload

--- dist/js/index.js
+++ dist/js/index.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
             },
         }
     })
-    
+
     $('.vouchersSlider').owlCarousel({
         loop: true,
         autoplay: true,
@@ -78,36 +78,74 @@ $(document).ready(function () {
         }
     })
 
-    
+
 
     $('.homeSlider.owl-carousel .owl-nav button.owl-prev').html('').html('<i class="fa-solid fa-chevron-left"></i>')
     $('.homeSlider.owl-carousel .owl-nav button.owl-next').html('').html('<i class="fa-solid fa-chevron-right"></i>')
 
     $('.favouritesSlider.owl-carousel .owl-nav button.owl-prev').html('').html('<i class="fa-solid fa-chevron-left"></i>')
     $('.favouritesSlider.owl-carousel .owl-nav button.owl-next').html('').html('<i class="fa-solid fa-chevron-right"></i>')
-   
+
     $('.vouchersSlider.owl-carousel .owl-nav button.owl-prev').html('').html('<i class="fa-solid fa-chevron-left"></i>')
     $('.vouchersSlider.owl-carousel .owl-nav button.owl-next').html('').html('<i class="fa-solid fa-chevron-right"></i>')
 
     $(".slideRight").click(function () {
-        var newScrollLeft = $(".mainSlidingScrollbar").scrollLeft() + $(".mainSlidingScrollbar").width() * 0.2;
-        console.log("New Scroll Left:", newScrollLeft);
-    
+        var containerWidth = $(".mainSlidingScrollbar").width();
+        var scrollPercentage = ($(window).width() < 640) ? 0.5 : 0.2;
+        var newScrollLeft = $(".mainSlidingScrollbar").scrollLeft() + containerWidth * scrollPercentage;
+
         $(".mainSlidingScrollbar").animate({ scrollLeft: newScrollLeft }, 500);
     });
 
     $(".slideLeft").click(function () {
-        var newScrollLeft = $(".mainSlidingScrollbar").scrollLeft() - $(".mainSlidingScrollbar").width() * 0.2;
-        console.log("New Scroll Left:", newScrollLeft);
-    
+        var containerWidth = $(".mainSlidingScrollbar").width();
+        var scrollPercentage = ($(window).width() < 640) ? 0.5 : 0.2;
+        var newScrollLeft = $(".mainSlidingScrollbar").scrollLeft() - containerWidth * scrollPercentage;
+
         $(".mainSlidingScrollbar").animate({ scrollLeft: newScrollLeft }, 500);
     });
-    
-    $(document).on('mouseover','.mailSlidingUl li.group',function(){
+
+
+    $(document).on('mouseover', '.mailSlidingUl li.group', function () {
         let height = $(this).find('ul').innerHeight();
-        $('.mailSlidingUl').height(height+30);
+        $('.mailSlidingUl').height(height + 30);
     })
-    
+
+    $(document).on('click', '.showMobileSearchBox', function () {
+        $('.mobileSearchWrapper').slideToggle()
+    })
+
+    $(document).on('click', '.headerSearchWrapper button', function () {
+        let SerchInput = $('.headerSearchWrapper input').val();
+        searchFunctionality(SerchInput)
+    })
+    $(document).on('click', '.mobileSearchWrapper button', function () {
+        let SerchInput = $('.mobileSearchWrapper input').val();
+        searchFunctionality(SerchInput)
+    })
+    $(document).on('click', '.newsLatterSubmit', function () {
+        let email = $('.footerNewslatter input').val();
+        if (email.trim() == '') {
+            $('.searchWrapper input').addClass('animate__animated animate__shakeX');
+            setTimeout(() => {
+                $('.searchWrapper input').removeClass('animate__animated animate__shakeX');
+            }, 1200)
+        } else {
+            // your ajax code
+        }
+    })
+
+    function searchFunctionality(SerchInput) {
+        if (SerchInput.trim() == '') {
+            $('.searchWrapper input').addClass('animate__animated animate__shakeX');
+            setTimeout(() => {
+                $('.searchWrapper input').removeClass('animate__animated animate__shakeX');
+            }, 1200)
+        } else {
+            // your ajax code
+        }
+    }
+
 
 
 });  // jquery function end
